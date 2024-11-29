50ce927a81427519a6cd928acbf7736731978118
nighthawk115 <williancesardepetriz@gmail.com>
2024-11-29 17:44:58 -0300

    0.0.3
    -playable character
    -auto tile

--- movable_object.gd
+++ movable_object.gd
@@ -1,5 +1,29 @@
 extends CharacterBody2D
 
+@onready var animation := $Anim as AnimatedSprite2D
+@onready var animation_tree := $AnimationTree as AnimationTree
+
+var state_machine
+
+func _ready() -> void:
+	state_machine = animation_tree["parameters/playback"]
+	
+
 func _process(delta: float) -> void:
-	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 350
+	move()
+	animate()
 	move_and_slide()
+
+func move() -> void:
+	var direction := Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	
+	velocity = direction.normalized() * 64
+	
+	if direction != Vector2.ZERO:
+		animation_tree["parameters/idle/blend_position"] = Vector2(direction.x, direction.y * (-1))
+		print("normalized",direction.normalized())
+		print(direction)
+
+func animate() -> void:
+	state_machine.travel("idle")
+	pass
